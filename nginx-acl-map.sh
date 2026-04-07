2ce952eddf775e369ca6238dc6427c5cbbab21c0
Robin Ellis <robin.cake.ellis@gmail.com>
2026-04-07 14:02:28 +0700

    tweak

--- nginx-acl-map.sh
+++ nginx-acl-map.sh
@@ -8,9 +8,9 @@ asn=${2:-}
 if [[ -z "$rule" ]] || [[ -z "$asn" ]]; then
     echo "error: invalid arguments"
     echo "usage:"
-    echo -e "\tnginx-acl.sh <rule> <asn>"
-    echo -e "\tnginx-acl.sh 0 14061"
-    echo -e "\tnginx-acl.sh 1 14061"
+    echo -e "\tnginx-acl-map.sh <rule> <asn>"
+    echo -e "\tnginx-acl-map.sh 0 14061"
+    echo -e "\tnginx-acl-map.sh 1 14061"
     exit 1
 fi
 
@@ -18,6 +18,5 @@ curl -sS --fail "https://stat.ripe.net/data/as-overview/data.json?resource=AS{$a
     | jq -r '"# AS" + .data.resource + ", " + .data.holder + ", " + .time' 
     
 curl -sS --fail "https://stat.ripe.net/data/announced-prefixes/data.json?resource=AS${asn}&sourceapp=nginx-acl-map" \
-  | jq -r '.data.prefixes[]
-  | select(.prefix | contains(":") | not)
-  | "\(.prefix) 1;"'
+  | jq -r '.data.prefixes[] | select(.prefix | contains(":") | not) | "\(.prefix)"' \
+  | xargs -I% echo "% ${rule};"
